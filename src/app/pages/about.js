1402312307c778cec01bde436be94156027a754f
tecnobert <mrrabbitomg@gmail.com>
2020-03-24 01:26:10 -0300

    About page started

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -4,15 +4,34 @@
  * https://github.com/tecnobert/konvertilo
  *****************************/
 
-import Container from '@material-ui/core/Container';
-import Nav from '../shared/nav'
-import React from 'react'
+import Container from "@material-ui/core/Container"
+import Grid from "@material-ui/core/Grid"
+import Nav from "../shared/nav"
+import React from "react"
+import Styled from "../../styles/styles"
+import { Typography } from "@material-ui/core"
 
 function About() {
-  return( 
-  <Container>
-       <Nav></Nav>
-       </Container>
- )
+  const classes = Styled()
+
+  return (
+    <Container>
+      <Nav></Nav>
+      <Container className={classes.about}>
+        <Grid item xs={6}>
+          <Typography variant="h4" component="h4" gutterBottom>
+            Binary converter
+          </Typography>
+          <Typography variant="p" component="p" gutterBottom>
+            A ideia de fazer um app que converta binário para decimal é um tanto
+            nostáugica e exitante, pelo menos para os devs que amam de verdade
+            programação e codificação.
+            
+          </Typography>
+         
+        </Grid>
+      </Container>
+    </Container>
+  )
 }
 export default About
