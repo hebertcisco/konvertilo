0d08b032e94f037499bc84cf62f8c7b0c6192d53
tecnobert <mrrabbitomg@gmail.com>
2020-03-24 01:25:50 -0300

    Snackbar added

--- src/app/binary.js
+++ src/app/binary.js
@@ -4,57 +4,45 @@
  * https://github.com/tecnobert/konvertilo
  *****************************/
 
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
-import ArrowDownwardTwoTone from '@material-ui/icons/ArrowDownwardTwoTone'
-import ComputerTwoTone from '@material-ui/icons/ComputerTwoTone'
-import Keyboard from '@material-ui/icons/Keyboard'
-import MuiAlert from '@material-ui/lab/Alert'
-import Snackbar from '@material-ui/core/Snackbar'
-import Styled from '../styles/styles'
-import { titles } from './app.json'
+import ArrowDownwardTwoTone from "@material-ui/icons/ArrowDownwardTwoTone"
+import Button from "@material-ui/core/Button"
+import CloseIcon from "@material-ui/icons/Close"
+import ComputerTwoTone from "@material-ui/icons/ComputerTwoTone"
+import IconButton from "@material-ui/core/IconButton"
+import Keyboard from "@material-ui/icons/Keyboard"
+import Snackbar from "@material-ui/core/Snackbar"
+import Styled from "../styles/styles"
+import { titles } from "./app.json"
 
 //import ArrowUpwardTwoTone from '@material-ui/icons/ArrowUpwardTwoTone'
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />
-}
+
 function BinaryMode() {
   let classes = Styled()
-  const [binaryText, setBinaryText] = useState('')
-  const [decimalText, setDecimalText] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-  
-  const [open, setOpen] = React.useState(false);
+  const [binaryText, setBinaryText] = useState("")
+  const [decimalText, setDecimalText] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
 
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
   // Perform the conversion on form submit
   const onFormSubmit = e => {
     e.preventDefault() // prevents refresh of the browser
 
     // Make sure we accept only either 0 or 1
     if (binaryText.match(/^[0-1]+$/g) === null) {
-      setErrorMessage('Enter either 0 or 1')
+      setErrorMessage("Enter either 0 or 1")
+      handleClick()
       return
     }
 
-    setErrorMessage('') // Reset the error message
+    setErrorMessage("") // Reset the error message
 
     // Formulae:
     // input = 1 => output = 1 * (2^0) = 1
     // input = 10 => output = (0 * (2^0)) + (1 * (2^1)) = 2
     // So we reverse and iterate from the back
     const reversedBinaryText = binaryText
-      .split('')
+      .split("")
       .map(Number) // Convert to a number from string
       .reverse()
 
@@ -65,21 +53,51 @@ function BinaryMode() {
     )
     setDecimalText(result)
   }
+  const [open, setOpen] = React.useState(false)
 
+  const handleClick = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return
+    }
+
+    setOpen(false)
+  }
   return (
     <div className="body">
       <h1 className={classes.h1}>{titles[0]}</h1>
 
       <form className={classes.StyledForm} onSubmit={onFormSubmit}>
         {errorMessage && (
-          <span className={classes.errorMessage}>{errorMessage}</span>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left"
+            }}
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message={errorMessage}
+            action={
+              <React.Fragment>
+                <Button color="secondary" size="small" onClick={handleClose}>
+                  OK
+                </Button>
+                <IconButton
+                  size="small"
+                  aria-label="close"
+                  color="inherit"
+                  onClick={handleClose}
+                >
+                  <CloseIcon fontSize="small" />
+                </IconButton>
+              </React.Fragment>
+            }
+          />
         )}
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success">
-          This is a success message!
-        </Alert>
-      </Snackbar>
-
         <br />
         <div className={classes.Field}>
           <label className={classes.Label}>
