9ca91e69ef7232309e9e02c959c104f5ed2bd71b
tecnobert <mrrabbitomg@gmail.com>
2020-05-27 14:49:23 -0300

    remove page unused

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import {
   Switch
 } from 'react-router-dom'
 
-import About from './app/pages/about'
 import App from './app/app'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -20,7 +19,6 @@ ReactDOM.render(
   <BrowserRouter keyLength={22}>
     <Switch>
       <Route path="/" exact={true} component={App} />
-      <Route path="/about" exact={true} component={About} />
       {/*<Redirect from="/" to="/binary" />*/}
       <Router basename="/map/5AJA3RefFuTZ8z4Gn6BjMgZRgPZ2"> </Router>
     </Switch>
