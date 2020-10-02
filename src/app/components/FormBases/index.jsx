12c185993fe388b198d57fb1ad919b8e8951848b
hebertcisco <mrrabbitomg@gmail.com>
2020-10-02 16:56:09 -0300

    Removed static typing from this project

--- src/app/components/FormBases/index.jsx
+++ src/app/components/FormBases/index.jsx
@@ -26,7 +26,7 @@ export const FormBases = ({
   const onFormSubmit = (e) => {
     e.preventDefault()
 
-  /*  if (baseText.match(/^[0-1]+$/g) === null) {
+    /*  if (baseText.match(/^[0-1]+$/g) === null) {
       setErrorMessage(error_message)
       handleClick()
       return
